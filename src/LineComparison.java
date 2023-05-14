f1bab10331d7a8845cf2c343ff6bf41b9b63a0b8
upendra <upendrasahu1199@gmail.com>
2023-05-15 03:10:29 +0530

    Added : Calculate Line length

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class LineComparison {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison Computation Program");
+
+    int lineNumber;
+    double firstPointX, firstPointY;
+    double secondPointX, secondPointY;
+
+    public LineComparison(int lineNumber) {
+        this.lineNumber = lineNumber;
+    }
+
+    public void enterCoordinate(double firstPointX, double firstPointY, double secondPointX, double secondPointY) {
+        this.firstPointX = firstPointX;
+        this.firstPointY = firstPointY;
+        this.secondPointX = secondPointX;
+        this.secondPointY = secondPointY;
+    }
+
+    public double calculatingLength() {
+
+        double length = Math.sqrt(Math.pow((secondPointX - firstPointX), 2) + Math.pow((secondPointY - firstPointY), 2));
+        System.out.println("Length of Line" + lineNumber + " = " + length);
+        return length;
     }
 }
---
+++ src/RunLineComparisonProgram.java
@@ -0,0 +1,8 @@
+public class RunLineComparisonProgram {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program");
+        LineComparison line1 = new LineComparison(1);
+        line1.enterCoordinate(1,5,4,3);
+        line1.calculatingLength();
+    }
+}
